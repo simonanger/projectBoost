1bf619b12f2d3167b45b33a5018871b23b674f87
Simon Anger <simonanger89@gmail.com>
2017-11-13 06:45:31 +0000

    Added some styling to levels

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -8,8 +8,7 @@ public class Oscillator : MonoBehaviour {
     [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f) ;
     [SerializeField] float period = 3f;
 
-    // todo remove from inspector later
-    [Range(0,1)] [SerializeField] float movementFactor; //0 for not moved 1 for fully moved
+    float movementFactor; //0 for not moved 1 for fully moved
     Vector3 startingPos;
 
 	// Use this for initialization
@@ -23,7 +22,7 @@ public class Oscillator : MonoBehaviour {
         float cycles = Time.time / period; //grows continually from 0
 
         const float tau = Mathf.PI * 2; // about 6.28
-        float rawSinWave = Mathf.Sin(cycles * tau);
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
 
         movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementFactor * movementVector;
