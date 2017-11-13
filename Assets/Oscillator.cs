934ebda2277da386bf5e3572f197d9214d01dc78
Simon Anger <simonanger89@gmail.com>
2017-11-13 06:32:03 +0000

    Oscillating platform level2

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f) ;
+    [SerializeField] float period = 3f;
 
     // todo remove from inspector later
     [Range(0,1)] [SerializeField] float movementFactor; //0 for not moved 1 for fully moved
@@ -18,6 +19,13 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        float cycles = Time.time / period; //grows continually from 0
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
 	}
